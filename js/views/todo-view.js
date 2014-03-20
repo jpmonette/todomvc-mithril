0a32dbce3af1ab0cc8b3ddad3ddad89a8b6cc6a0
Jean-Philippe Monette <contact@jpmonette.net>
2014-03-20 19:36:19 -0400

    seperating footer

--- js/views/todo-view.js
+++ js/views/todo-view.js
@@ -45,34 +45,7 @@ var app = app || {};
                      })
                 ])
             ]),
-            ctrl.list.length == 0 ? '' : m('footer#footer', [
-                m('span#todo-count', [
-                    m('strong', ctrl.list.length), ' item' + (ctrl.list.length > 1 ? 's' : '') + ' left'
-                ]),
-                m('ul#filters', [
-                    m('li', [
-                        m('a[href=/]', {
-                            config: m.route,
-                            class: ctrl.filter() == '' ? 'selected' : ''
-                        }, 'All')
-                    ]),
-                    m('li', [
-                        m('a[href=/active]', {
-                            config: m.route,
-                            class: ctrl.filter() == 'active' ? 'selected' : ''
-                        }, 'Active')
-                    ]),
-                    m('li', [
-                        m('a[href=/completed]', {
-                            config: m.route,
-                            class: ctrl.filter() == 'completed' ? 'selected' : ''
-                        }, 'Completed')
-                    ])
-                ]),
-                ctrl.amountCompleted() == 0 ? '' : m('button#clear-completed', {
-                    onclick: ctrl.clearCompleted.bind(ctrl)
-                }, 'Clear completed (' + ctrl.amountCompleted() + ')')
-            ])
+            ctrl.list.length == 0 ? '' : app.footer(ctrl)
         ];
     };
 
